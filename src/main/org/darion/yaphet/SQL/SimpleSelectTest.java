5ae31398dad9471d3e7367741e9538e9bff993ba
darionyaphet <darion.yaphet@gmail.com>
2016-09-15 10:44:24 +0800

    daily publish

--- src/main/org/darion/yaphet/SQL/SimpleSelectTest.java
+++ src/main/org/darion/yaphet/SQL/SimpleSelectTest.java
@@ -1,10 +1,10 @@
 package org.darion.yaphet.SQL;
 
 /**
- * Created by MLS on 15/9/23.
+ * Created by darion.yaphet on 15/9/23.
  */
 public class SimpleSelectTest {
     public static void main(String[] args) {
-
+        System.out.println("start");
     }
 }
